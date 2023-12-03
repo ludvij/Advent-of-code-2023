5410817a4e3679c7e8092dd3dfb0ace4b7879a27
ludvij <vijande48@gmail.com>
2023-12-03 05:42:22 +0100

    added alternative solution

--- day_1/src/day1.cpp
+++ day_1/src/day1.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <regex>
 #include <vector>
+#include <list>
 
 
 struct Lookup {
-	std::string_view str_version;
-	int32_t number_version;
+	const std::string_view str;
+	const int32_t number;
 };
 
 std::vector<Lookup> lookups {
@@ -22,36 +21,34 @@ std::vector<Lookup> lookups {
 	{"9", 9}, {"nine",  9},
 };
 
+
 uint32_t get_calibration(std::string filename)
 {
 	std::ifstream file(filename);
-	std::string line;
 
 	uint32_t res = 0;
 
-	while (std::getline(file, line)) {
+	for (std::string line; std::getline(file, line);) {
 		uint32_t first_integer = 0;
 		uint32_t first_integer_pos = UINT32_MAX;
 		uint32_t second_integer = 0;
 		uint32_t second_integer_pos = 0;
 
 		for (const auto& lkup : lookups) {
-			if (const size_t pos = line.find(lkup.str_version); pos != std::string::npos) {
+			if (const size_t pos = line.find(lkup.str); pos != std::string::npos) {
 				if (pos < first_integer_pos) {
 					first_integer_pos = pos;
-					first_integer = lkup.number_version;
+					first_integer = lkup.number;
 				}
 			}
-			if (const size_t pos = line.rfind(lkup.str_version); pos != std::string::npos) {
+			if (const size_t pos = line.rfind(lkup.str); pos != std::string::npos) {
 				if (pos >= second_integer_pos) {
 					second_integer_pos = pos;
-					second_integer = lkup.number_version;
+					second_integer = lkup.number;
 				}
 			}
 		}
 		res += first_integer * 10 + second_integer;
-		std::cout << line << '\n';
-		std::cout << "  " << first_integer << second_integer << '\n';
 	}
 
 	return res;
@@ -63,7 +60,6 @@ int main(int argc, char **argv)
 		std::cout << "Usage: " << argv[0] << "path/to/file\n";
 		return 0;
 	}
-	
 	const uint32_t res = get_calibration(argv[1]);
 	std::cout << "Total: " << res << '\n';
 	
