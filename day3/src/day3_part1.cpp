4b15cfdd2d07f8aab25ca0f138d60b8962a8f070
ludvij <vijande48@gmail.com>
2023-12-05 14:39:59 +0100

    day 3 full, harnessing the power of the unordered map

--- day3/src/day3_part1.cpp
+++ day3/src/day3_part1.cpp
@@ -1,109 +1,55 @@
 #include <iostream>
-#include <filesystem>
 #include <fstream>
 #include <string>
 #include <vector>
-#include <deque>
 
 
-struct Cell {
-	int32_t value = 0;
-	int32_t pos = 0;
-	int32_t size = 0;
-	bool found = false;
 
-	enum class Type {
-		Symbol,
-		Number,
-		Empty,
-	} type = Type::Empty;
-};
-
-
-std::vector<Cell> make_cells(std::string line)
-{
-	Cell candidate;
-	std::vector<Cell> current;
-	current.resize(line.length());
-	for (size_t i = 0; i < line.length(); i++) {
-		const char c = line[i];
-		if (std::isdigit(c)) {
-			if (candidate.type == Cell::Type::Empty) {
-				candidate.pos = i;
-				candidate.type = Cell::Type::Number;
-			}
-			candidate.value = candidate.value * 10 + (c - 0x30);
-			candidate.size++;
-		}
-		else {
-			if (candidate.type != Cell::Type::Empty) {
-				current[candidate.pos] = std::move(candidate);
-				candidate = Cell();
-			}
-			if (c != '.') {
-				current[i] = {0, static_cast<int32_t>(i), 1, false, Cell::Type::Symbol};
-			}
-		}
-	}
-	if (candidate.type != Cell::Type::Empty) {
-		current[candidate.pos] = std::move(candidate);
-	}
-
-	return current;
-}
-
-
-uint32_t cross_compare(std::vector<Cell>& v1, const std::vector<Cell>& v2)
+uint32_t find_parts(const std::vector<std::string>& lines) 
 {
 	uint32_t res = 0;
-	for (size_t i = 0; i < v1.size(); i++) {
-		auto& cell = v1[i];
-		if (!(cell.type == Cell::Type::Number && !cell.found)) {
-			continue;
-		}
-			
-		size_t begin_x = i == 0 ? i : i - 1;
-		size_t end_x = (i + cell.size) == v1.size() ? i + cell.size - 1: i + cell.size;
-		if (v1[begin_x].type == Cell::Type::Symbol || v1[end_x].type == Cell::Type::Symbol) {
-			res += cell.value;
-			cell.found = true;
-		}
-		for (size_t j = begin_x; j <= end_x && j < v2.size(); j++) {
-			if (!cell.found && v2[j].type == Cell::Type::Symbol) {
-				res += cell.value;
-				cell.found = true;
-			} 
-		}
-		if (cell.found) {
-			std::cout << "  [FOUND] " <<cell.value << " ["<<begin_x <<", "<< end_x << "]" <<'\n';
+	for(size_t y = 0; y < lines.size(); y++) {
+		for(size_t x = 0; x < lines[y].length(); x++) {
+			if (!std::isdigit(lines[y][x])) {
+				continue;
+			}
+			int32_t number = 0;
+			size_t x0 = x == 0 ? x : x - 1;
+			size_t y0 = y == 0 ? y : y - 1;
+			for (size_t found = x; found < lines[y].length(); found++) {
+				if (!std::isdigit(lines[y][found])) {
+					break;
+				}
+				number = number * 10 + (lines[y][found] - 0x30);
+				x++;
+			}
+			size_t x1 = x == lines[y].size() ? x : x + 1;
+			size_t y1 = (y + 1) == lines.size() ? y : y + 1;
+			bool found = false;
+			for (size_t fy = y0; fy <= y1; fy++) {
+				for (size_t fx = x0; fx < x1; fx++) {
+					if (lines[fy][fx] != '.' && !std::isdigit(lines[fy][fx])) {
+						res += number;
+						found = true;
+					}
+					if (found) break;
+				}
+				if (found) break;
+			}
 		}
 	}
 	return res;
 }
 
-
-uint32_t find_parts(const char* filename) 
+std::vector<std::string> slurp(const char* filename) 
 {
 	std::ifstream file(filename);
-	uint32_t res = 0;
-
-	std::deque<std::vector<Cell>> lines;
-	std::string past_line;
-	for(std::string line; getline(file, line);) {
-		lines.push_back(make_cells(line));
-		if (lines.size() > 2) {
-			lines.pop_front();
-		}
-		std::cout << "Comparing: " << past_line << '\n';
-		std::cout << "Against:   " << line << '\n';
-		res += cross_compare(lines.front(), lines.back());
-
-		std::cout << "Comparing: " << line << '\n';
-		std::cout << "Against:   " << past_line << '\n';
-		res += cross_compare(lines.back(), lines.front());
-	
+	std::vector<std::string> lines;
+	for(std::string line; std::getline(file, line);) {
+		lines.push_back(std::move(line));
 	}
-	return res;
+
+	return lines;
 }
 
 
@@ -113,7 +59,8 @@ int main(int argc, char** argv)
 		std::cout << "Usage " << argv[0] << "path/to/file\n";
 		return 0;
 	}
-	uint32_t res = find_parts(argv[1]);
+	auto lines = slurp(argv[1]);
+	const uint32_t res = find_parts(lines);
 	std::cout << "Total: " << res << '\n';
 	return 0;
 }
--- day3/src/day3_part2.cpp
+++ day3/src/day3_part2.cpp
@@ -1,109 +1,77 @@
 #include <iostream>
-#include <filesystem>
 #include <fstream>
 #include <string>
 #include <vector>
-#include <deque>
+#include <unordered_map>
 
+struct Point {
+	size_t x;
+	size_t y;
+	friend bool operator==(const Point& lhs, const Point& rhs) {
+		return rhs.x == lhs.x && rhs.y == lhs.y;
+	}
+};
 
-struct Cell {
-	int32_t value = 0;
-	int32_t pos = 0;
-	int32_t size = 0;
-	bool found = false;
+template <> struct std::hash<Point> {
+	size_t operator()(const Point& s) const noexcept {
+		const size_t x = std::hash<size_t>{}(s.x);
+		const size_t y = std::hash<size_t>{}(s.y);
 
-	enum class Type {
-		Symbol,
-		Number,
-		Empty,
-	} type = Type::Empty;
+		return x ^ (y << 1);
+	}
 };
 
 
-std::vector<Cell> make_cells(std::string line)
+uint32_t find_parts(const std::vector<std::string>& lines) 
 {
-	Cell candidate;
-	std::vector<Cell> current;
-	current.resize(line.length());
-	for (size_t i = 0; i < line.length(); i++) {
-		const char c = line[i];
-		if (std::isdigit(c)) {
-			if (candidate.type == Cell::Type::Empty) {
-				candidate.pos = i;
-				candidate.type = Cell::Type::Number;
-			}
-			candidate.value = candidate.value * 10 + (c - 0x30);
-			candidate.size++;
-		}
-		else {
-			if (candidate.type != Cell::Type::Empty) {
-				current[candidate.pos] = std::move(candidate);
-				candidate = Cell();
-			}
-			if (c == '*') {
-				current[i] = {0, static_cast<int32_t>(i), 1, false, Cell::Type::Symbol};
-			}
-		}
-	}
-	if (candidate.type != Cell::Type::Empty) {
-		current[candidate.pos] = std::move(candidate);
-	}
-
-	return current;
-}
-
-
-uint32_t cross_compare(std::vector<Cell>& v1, const std::vector<Cell>& v2)
-{
+	std::unordered_map<Point, int32_t> cogs;
 	uint32_t res = 0;
-	for (size_t i = 0; i < v1.size(); i++) {
-		auto& cell = v1[i];
-		if (!(cell.type == Cell::Type::Number && !cell.found)) {
-			continue;
-		}
-			
-		size_t begin_x = i == 0 ? i : i - 1;
-		size_t end_x = (i + cell.size) == v1.size() ? i + cell.size - 1: i + cell.size;
-		if (v1[begin_x].type == Cell::Type::Symbol || v1[end_x].type == Cell::Type::Symbol) {
-			res += cell.value;
-			cell.found = true;
-		}
-		for (size_t j = begin_x; j <= end_x && j < v2.size(); j++) {
-			if (!cell.found && v2[j].type == Cell::Type::Symbol) {
-				res += cell.value;
-				cell.found = true;
-			} 
-		}
-		if (cell.found) {
-			std::cout << "  [FOUND] " <<cell.value << " ["<<begin_x <<", "<< end_x << "]" <<'\n';
+	for(size_t y = 0; y < lines.size(); y++) {
+		for(size_t x = 0; x < lines[y].length(); x++) {
+			if (!std::isdigit(lines[y][x])) {
+				continue;
+			}
+			int32_t number = 0;
+			const size_t x0 = x == 0 ? x : x - 1;
+			const size_t y0 = y == 0 ? y : y - 1;
+			for (size_t found = x; found < lines[y].length(); found++) {
+				if (!std::isdigit(lines[y][found])) {
+					break;
+				}
+				number = number * 10 + (lines[y][found] - 0x30);
+				x++;
+			}
+			const size_t x1 = x == lines[y].size() ? x : x + 1;
+			const size_t y1 = (y + 1) == lines.size() ? y : y + 1;
+			bool found = false;
+			for (size_t fy = y0; fy <= y1; fy++) {
+				for (size_t fx = x0; fx < x1; fx++) {
+					if (lines[fy][fx] == '*') {
+						if (cogs.contains({fx, fy})) {
+							res += cogs.at({fx, fy}) * number;
+						}
+						else {
+							cogs.try_emplace({fx, fy}, number);
+						}
+					}
+					if (found) break;
+				}
+				if (found) break;
+			}
 		}
 	}
 	return res;
 }
 
-
-uint32_t find_parts(const char* filename) 
+std::vector<std::string> slurp(const char* filename) 
 {
 	std::ifstream file(filename);
-	uint32_t res = 0;
-
-	std::deque<std::vector<Cell>> lines;
-	std::string past_line;
-	for(std::string line; getline(file, line);) {
-		lines.push_back(make_cells(line));
-		if (lines.size() > 2) {
-			lines.pop_front();
-		}
-		std::cout << "Comparing: " << past_line << '\n';
-		std::cout << "Against:   " << line << '\n';
-		res += cross_compare(lines.front(), lines.back());
-
-		std::cout << "Comparing: " << line << '\n';
-		std::cout << "Against:   " << past_line << '\n';
-		res += cross_compare(lines.back(), lines.front());
-	
+	std::vector<std::string> lines;
+	for(std::string line; std::getline(file, line);) {
+		lines.push_back(line);
 	}
-	return res;
+
+	return lines;
 }
 
 
@@ -113,7 +81,8 @@ int main(int argc, char** argv)
 		std::cout << "Usage " << argv[0] << "path/to/file\n";
 		return 0;
 	}
-	uint32_t res = find_parts(argv[1]);
+	auto lines = slurp(argv[1]);
+	const uint32_t res = find_parts(lines);
 	std::cout << "Total: " << res << '\n';
 	return 0;
 }
