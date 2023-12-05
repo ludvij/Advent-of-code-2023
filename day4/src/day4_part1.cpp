99e8abb2c4fcc62c8127d015cf39b95f6c72eb87
ludvij <vijande48@gmail.com>
2023-12-05 16:26:04 +0100

    day 4 part 1

---
+++ day4/src/day4_part1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+#include "ctre.hpp"
+
+uint32_t do_cards(const char* filename) {
+	std::ifstream file(filename);
+	uint32_t res = 0;
+	for(std::string line; std::getline(file, line);) {
+		int32_t count = 0;
+		std::unordered_set<std::string_view> winning_set;
+		auto [whole, winning, have] = ctre::match<".+: (.+) \\| (.+)">(line);
+		for (auto match : ctre::search_all<"(\\d+)">(winning)) {
+			winning_set.insert(match.view());
+		}
+		for (auto match : ctre::search_all<"(\\d+)">(have)) {
+			if (winning_set.contains(match.view())) {
+				count = count == 0 ? 1 : count * 2;
+			}
+		}
+		res += count;
+	}
+	return res;
+}
+
+int main(int argc, char** argv)
+{
+	if (argc < 2) {
+		std::cout << "Usage: " << argv[0] << " path/to/file\n";
+	}
+	const uint32_t res = do_cards(argv[1]);
+	std::cout << "Total: " << res << '\n';
+}
