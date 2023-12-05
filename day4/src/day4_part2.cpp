e507bbb73e5c5c25537b29f52763e44410651a52
ludvij <vijande48@gmail.com>
2023-12-05 20:20:57 +0100

    day 4

--- day4/src/day4_part2.cpp
+++ day4/src/day4_part2.cpp
@@ -4,10 +4,11 @@
 #include <vector>
 #include <string_view>
 #include <unordered_set>
+#include <vector>
+#include <algorithm>
 #include "ctre.hpp"
 
-
-uint32_t calculate_next_cards(std::string line) 
+uint32_t calculate_wins(const std::string& line) 
 {
 	int32_t count = 0;
 	std::unordered_set<std::string_view> winning_set;
@@ -23,15 +24,38 @@ uint32_t calculate_next_cards(std::string line)
 	return count;
 }
 
-uint32_t do_cards(std::vector<std::string> data) 
+// 1 -> 2 3 4 5 -> 3 4 4 5 5 -> 4 5 5 5 -> 5, 4 + 6 + 3 + 1 = 14
+// 2 -> 3 4 -> 4 5 5 -> 5, 2 + 3 + 1 = 6
+// 3 -> 4 5 -> 5, 2 + 1 = 3
+// 4 -> 5, 1
+// 5 
+// 6
+uint32_t do_cards(const std::vector<std::string>& data) 
 {
+	// cache wins
 	uint32_t res = 0;
-	for(size_t i = 0; i < data.size(); i++) {
-		res += calculate_next_cards(data[i]);
-	}
+	std::vector<uint32_t> wins;
+	wins.resize(data.size());
 
+	for (size_t i = data.size(); i > 0; i--) {
+		const uint32_t card_wins = calculate_wins(data[i - 1]);
+		std::cout << "Card: " << i - 1 << "\n wins: " << card_wins;
+		wins[i - 1] = card_wins;
+		for(uint32_t j = 0; j < card_wins; j++) {
+			if (i + j >= wins.size()) {
+				break;
+			}
+			std::cout << " + " <<"["<< i + j<<"]"<< wins[i + j];
+			wins[i - 1] += wins[i + j];
+		}
+		std::cout << " = " << wins[i - 1] <<'\n';
+	}
+	for (size_t i = 0; i < wins.size(); i++) {
+		res += wins[i] + 1;
+	}
 	return res;
 }
+
 
 std::vector<std::string> slurp(const char* filename)
 {
