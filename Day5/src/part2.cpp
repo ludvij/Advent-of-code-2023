205a9fa2875fa0161a5af03b3112ca27e1ae3498
ludvij <vijande48@gmail.com>
2023-12-07 03:35:32 +0100

    day 5 done, kinda of a bruteforcer, but it is my bruteforcer

--- Day5/src/part2.cpp
+++ Day5/src/part2.cpp
@@ -22,9 +22,19 @@ public:
 		return source >= from && source < (from + size);
 	}
 
+	bool rHasRange(const uint64_t destination) const 
+	{
+		return destination >= to && destination < (to + size);
+	}
+
 	uint64_t Convert(const uint64_t source) const
 	{
 		return source - converter;
+	}
+
+	uint64_t rConvert(const uint64_t destination) const 
+	{
+		return destination + converter;
 	}
 
 	int64_t converter;
@@ -34,44 +44,22 @@ public:
 };
 
 
-class Mapper_set {
-public:
-
-	void AppendMap(const uint64_t source, const uint64_t destination, const uint64_t range)
-	{
-		mappers.emplace_back(source, destination, range);
-	}
-
-	uint64_t Convert(const uint64_t source) const
-	{
-		for(const auto& mapper: mappers) {
-			if (mapper.HasRange(source)) {
-				return mapper.Convert(source);
-			}
-		}
-
-		return source;
-	}
-
-	std::vector<Mapper> mappers;
-};
 
 
 
-
-std::vector<Mapper_set> make_mappers(std::ifstream& file)
+std::vector<std::vector<Mapper>> make_mappers(std::ifstream& file)
 {
-	std::vector<Mapper_set> mappers;
+	std::vector<std::vector<Mapper>> mappers;
 	for(std::string line; std::getline(file, line);) {
 		if (line.empty()) {
 			continue;
 		}
-		else if (const auto [match, from, to] = ctre::match<"(\\w+)-to-(\\w+) map:">(line); match) {
+		else if (ctre::match<"\\w+-to-\\w+ map:">(line)) {
 			mappers.emplace_back();
 			continue;
 		}
 		const auto [match, destination, source, range] = ctre::match<"(\\d+) (\\d+) (\\d+)">(line);
-		mappers.back().AppendMap(
+		mappers.back().emplace_back(
 			std::stoull(source.str()), 
 			std::stoull(destination.str()), 
 			std::stoull(range.str())
@@ -82,32 +70,40 @@ std::vector<Mapper_set> make_mappers(std::ifstream& file)
 }
 
 
-uint32_t do_seeds(const char* filename) 
+uint64_t do_seeds(const char* filename) 
 {
-	struct Val_range {
-		uint64_t begin;
-		uint64_t size;
-	}
+
 	std::ifstream file(filename);
-	std::vector<Val_range> vals;
+	std::vector<Mapper> seeds;
 	std::string seeds_string;
+
 	std::getline(file, seeds_string);
-	for(const auto match : ctre::search_all<"(\\d+) (\\d+)">(seeds_string)) {
-		vals.emplace_back(std::stoul(match.get<1>().str()), std::stoul(match.get<2>().str()));
+
+	for(const auto& match : ctre::search_all<"(\\d+) (\\d+)">(seeds_string)) {
+		seeds.emplace_back(std::stoul(match.get<1>().str()), 0, std::stoul(match.get<2>().str()));
 	}
 
 	const auto mappers = make_mappers(file);
 
-	uint64_t res = UINT32_MAX;
-
-	for(auto& val : vals) {
-		for (const auto& mapper : mappers) {
-		
+	uint64_t res = 0;
+	while(1) {
+		size_t start = res;
+		for(auto it = mappers.rbegin(); it != mappers.rend(); ++it) {
+			for (const auto& mapper : *it) {
+				if (mapper.rHasRange(start)) {
+					start = mapper.rConvert(start);
+					break;
+				}
+			}
 		}
-		std::cout << '\n';
-	}
 
-	return res;
+		for (const auto& val: seeds) {
+			if (val.HasRange(start)) {
+				return res;
+			}
+		}
+		res++;
+	}
 }
 
 
@@ -118,6 +114,6 @@ int main(int argc, char** argv)
 		std::cout << "Usage: " << argv[0] << " path/to/file\n";
 		return 0;
 	}
-	const uint32_t res = do_seeds(argv[1]);
+	const uint64_t res = do_seeds(argv[1]);
 	std::cout << "Total: " << res << '\n';
 }
