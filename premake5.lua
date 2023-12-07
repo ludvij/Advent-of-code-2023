8c45fc73ed8d57d96d0f9da5df141abe13e3a992
ludvij <vijande48@gmail.com>
2023-12-07 05:04:22 +0100

    aaaa

--- premake5.lua
+++ premake5.lua
@@ -1,27 +1,6 @@
-workspace "AoC_2023"
-	architecture "x86_64"
-
-	flags {
-		"MultiProcessorCompile",
-	}
-
-	buildoptions {
-		"-Wall", 
-		"-Wextra", 
-		"-pedantic", 
-		"-Wshadow",
-		"-Wnon-virtual-dtor"
-	}
-
-	configurations {
-		"Part1",
-		"Part2",
-	}
-
-
-
 include "Day1"
 include "Day2"
 include "Day3"
 include "Day4"
--- include "Day5"
+include "Day5"
+include "Day6"
